1ce9e648a22ee906c977843910553b51ca03d125
Calasada <carlyodes1@gmail.com>
2017-06-29 23:32:36 -0400

    Imported + Formatted Old SwerveDriveBase Subsystem
    Nearly identical to 2017's swerve code, with formatting including
    constants and moving of variables.

--- src/org/usfirst/frc2783/robot/OI.java
+++ src/org/usfirst/frc2783/robot/OI.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc2783.robot;
- 
+
+import edu.wpi.first.wpilibj.Joystick;
+
 public class OI {
+	
+	public static Joystick driver = new Joystick(Constants.kDriverControllerId);
 
     public OI() {
     }
